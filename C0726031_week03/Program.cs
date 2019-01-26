fb9deeea090ae50ff0310e65012fca455ce4455a
visualstudio <visualstudio@122-PC09>
2019-01-25 20:36:33 -0500

    villages

--- C0726031_week03/Program.cs
+++ C0726031_week03/Program.cs
@@ -21,6 +21,8 @@ namespace C0726031_week03
             Village NextVillage;
             String villageName;
             bool isAstrildeHere = false;
+            bool mexicoHere = false;
+            bool islandHere = false;
             public Village() { }
         }
 
