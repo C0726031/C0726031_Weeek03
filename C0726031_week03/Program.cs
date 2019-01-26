0701057d591424366bfb5027f6d9d0b365c43c9e
visualstudio <visualstudio@122-PC09>
2019-01-25 21:01:52 -0500

    differnet villages

--- C0726031_week03/Program.cs
+++ C0726031_week03/Program.cs
@@ -8,22 +8,46 @@ namespace C0726031_week03
 {
     class Program
     {
+        static void Main(string[] args)
+        { }
         class Countryside
         {
-            //how will I make some Villages?
+            Village Alst;
+            Village Maeland;
+            Village Scheing;
+
+            public void InitializeMap()
+            {
+                Alst = new Village();
+                Maeland = new Village();
+                Maeland.isAstrildeHere = true;
+                Scheing = new Village();
+
+                Alst.NextVillage = Maeland;
+                Maeland.NextVillage = Scheing;
+                Scheing.NextVillage = Alst;
+            }
+            public void WalkAround()
+            {
+                Village InitialVillage = this.Maeland;
+
+
+            }
+
+
+
+            class Village
+
+            {
+                public Village PreviousVillage;
+                public Village NextVillage;
+                public String villageName;
+                public bool isAstrildeHere = false;
+                bool isScheingHere = false;
+                public Village() { }
+            }
 
         }
-
-        class Village
-
-        {
-            Village PreviousVillage;
-            Village NextVillage;
-            String villageName;
-            bool isAstrildeHere = false;
-            bool islandHere = false;
-            public Village() { }
-        }
-
     }
 }
+
